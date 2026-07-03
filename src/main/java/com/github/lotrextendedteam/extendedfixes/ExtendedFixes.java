9822df398c9e2e688e95cfe7b8c7830627950b43
maximuslotro <61996354+maximuslotro@users.noreply.github.com>
2026-07-03 18:25:15 -0500

    Add initial mod

---
+++ src/main/java/com/github/lotrextendedteam/extendedfixes/ExtendedFixes.java
@@ -0,0 +1,10 @@
+package com.github.lotrextendedteam.extendedfixes;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("extended_patches")
+public class ExtendedFixes {
+
+    public ExtendedFixes() {
+    }
+}
